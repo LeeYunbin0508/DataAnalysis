12ae20cb963907fcff5162d1076946bd15e909b9
Yunbin <154854845+LeeYunbin0508@users.noreply.github.com>
2024-01-02 15:16:41 +0900

    Update 데이터분석_리스트.py

--- 데이터분석_리스트.py
+++ 데이터분석_리스트.py
@@ -2,14 +2,9 @@
 # coding: utf-8
 
 # In[2]:
-
-
 import pandas as pd 
 
-
 # In[20]:
-
-
 data = {
     '이름' : ['홍길동', '정대만', '송경영', '서코딩', '강백호', '변인정', '황태산', '윤행복'],
     '학교' : ['부산고', '부산고', '부산고', '부산고', '부산고', '경남고', '경남고', '경남고'],
@@ -24,101 +19,28 @@ data = {
 df = pd.DataFrame(data, index=['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번'])
 df
 
-
 # In[21]:
-
-
 data['이름'] # 사전구조는 key, value로 구성 
 
-
 # In[22]:
-
-
 df.index.name = '지원번호'
 df
 
-
 # In[19]:
-
-
 df.reset_index(drop=True) # 원래 쓰던 '지원번호' 인덱스 삭제 (drop=True)
 
-
 # In[23]:
-
-
 df.reset_index() # 새로운 인덱스가 생성되고, 기존 지원번호 인덱스는 새로운 column으로 변경됨 
 
-
 # In[24]:
-
-
 df.reset_index(drop=True, inplace=True)
 df
 
-
 # In[25]:
-
-
 df.set_index('이름') # 인덱스를 이름으로 설정
 
-
 # In[26]:
-
-
 df.sort_index() # 올림 차순으로 인덱스 정렬 (0부터 7까지)
 
-
 # In[27]:
-
-
 df.sort_index(ascending=False) # 내림 차순으로 인덱스 정렬 (7부터 0까지)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
